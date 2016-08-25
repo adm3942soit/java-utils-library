4985647a1d1aaca38d9cedc25224c95a03747edf
Oksana Dudnik <oksana.dudnik@lpb.lv>
2016-08-25 13:57:34 +0300

    added DateUtil

---
+++ sources/java-utils-library/src/main/java/com/utils/date/DateUtil.java
@@ -0,0 +1,61 @@
+package com.utils.date;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class DateUtil {
+
+   // List of all date formats that we want to parse.
+   // Add your own format here.
+   public static List<SimpleDateFormat>
+           dateFormats = new ArrayList<SimpleDateFormat>() {
+       private static final long serialVersionUID = 1L;
+       {
+           add(new SimpleDateFormat("M/dd/yyyy"));
+           add(new SimpleDateFormat("dd.M.yyyy"));
+           add(new SimpleDateFormat("M/dd/yyyy hh:mm:ss a"));
+           add(new SimpleDateFormat("dd.M.yyyy hh:mm:ss a"));
+           add(new SimpleDateFormat("dd.MMM.yyyy"));
+           add(new SimpleDateFormat("dd-MMM-yyyy"));
+           add(new SimpleDateFormat("yyyy-MM-dd"));
+           add(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss"));
+           add(new SimpleDateFormat("yyyy-mmmm-dd hh:mm:ss"));
+           add(new SimpleDateFormat("yyyy-mmmm-dd"));
+
+       }
+   };
+
+   /**
+    * Convert String with various formats into java.util.Date
+    *
+    * @param input
+    *            Date as a string
+    * @return java.util.Date object if input string is parsed
+    *          successfully else returns null
+    */
+   public static Date convertToDate(String input) {
+       Date date = null;
+       if(null == input) {
+           return null;
+       }
+       for (SimpleDateFormat format : dateFormats) {
+           try {
+               format.setLenient(false);
+               date = format.parse(input);
+           } catch (ParseException e) {
+               // try other formats
+            //System.out.println("ParseException"+e.getMessage());
+           }
+           if (date != null) {
+               break;
+           }
+       }
+
+       return date;
+   }
+
+}
